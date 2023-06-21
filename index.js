f4420f98aed1169369b09edbde1c0cd6a200da45
BalashovaMa <balashova.m.nd@gmail.com>
2023-06-21 09:54:20 +0300

    feat: add initial settings

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-const args = process.argv[2];
-const userName = args.split('=')[1];
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+
+//Get the home directory of the current user
+const homeDirectory = process.env.HOME || process.env.USERPROFILE;
+
+//Get the current working directory
+const currentDirectory = process.cwd();
+
+function printCurrentDirectory() {
+    console.log(`You are currently in ${currentDirectory}`);
+}
+
+printCurrentDirectory();
+
+const args = process.argv.slice(2);
+const usernameArg = args.find(arg => arg.startsWith('--username='));
+const userName = usernameArg ? usernameArg.split('=')[1] : '';
+
+if (!userName) {
+    console.error('Error: Username is missing.');
+    console.log('Please provide the --username argument when starting the program.');
+    process.exit(1);
+}
+
 
 console.log(`Welcome to the File Manager, ${userName}!`);
+
+process.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+});
